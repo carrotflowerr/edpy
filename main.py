cb2288d52e32db044b9b599a016ca2d06e7f5a10
carrotflowerr <64298096+carrotflowerr@users.noreply.github.com>
2023-10-21 18:28:13 -0600

    Update main.py
    added some code lol

--- main.py
+++ main.py
@@ -1,40 +1,94 @@
-#
 import os
 
+input_lines = []
+# this is our buffer (highly cringe)
+filename = 'out.txt'
+
 def main():
-    q=0
+    global lines
+    global filename
+    lines = []
+    
+    with open(filename, 'r') as file:
+        for line in file:
+            lines.append(line.strip())
+    #print(lines)   
+    q = 0
     while q == 0:
-        mode=input('>')
-        if mode == 'i': write()
+        mode = input('>')
+        if mode == 'i': insert()
+        if mode == 'w': write()
         if mode == 'p': print_file()
+        if mode == 'd': deleteLine() # d:{line_number}
         if mode == '!': command()
+        if mode == 'f': name_file()
         if mode == 'cl': clear()
-        if mode == 'q': q=1
-        #else: print('?')
-        # jank?
-def write():    
+        if mode == 'q': q = 1
+
+
+def name_file():
+    global filename
+    print("Current file:",filename)
+    new_file_name = str(input("Filename:"))
+    filename = new_file_name
+
+def insert():
+    global input_lines 
     while True:
-        #print('insert\n')
         current_line = input('')
         if current_line == '.':
             break
-        with open('out.txt', 'a') as file:
-            file.write(str(current_line)+"\n")
-            
-def clear():
-    with open('out.txt', 'w') as file:
-        file.write("")
-    # This is bad.
-    
-    
-def print_file():
-    os.system("cat out.txt")
+        input_lines.append(current_line)  
 
+
+
+def deleteLine():
+    global lines
+    global filename
+    print(len(lines)-1)
+    # puts lines in an array
+
+    query = int(input())# 1=1
+    #query = query + 1
+    if query > len(lines):
+        print('out of range')
+    if query <= len(lines):
+        searched_line = (lines[query])
+        print(searched_line)
+        edit_choice = str(input())
+        if edit_choice == 'q': 
+            return lines
+        if edit_choice == 'd': 
+            searched_line = lines.pop(query)  # Remove and store the line to be deleted
+            print(f'Deleted: {searched_line}')
+            with open(filename, 'w') as file:
+                file.write('\n'.join(lines))
+    
+
+def write():
+    global input_lines  
+    global filename
+    with open(filename, 'a') as file:
+        for line in input_lines:
+            file.write(line + '\n')
+    input_lines = []  # Clear the list after writing
+
+def clear():
+    global filename
+    with open(filename, 'w') as file:
+        file.write("")
+
+def print_file():
+    global filename
+    os.system('cat '+str(filename))
+   
 def command():
-    q=0
+    global filename
+    q = 0
     while q == 0:
         command = str(input('!'))
-        if command == 'q':break
+        if command == 'q':
+            break
         os.system(command)
 
 main()
